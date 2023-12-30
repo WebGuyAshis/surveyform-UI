20d5c3677ab1a2bc02febdf20a550cf94528292c
Ashis <developer.ashis99@gmail.com>
2023-12-30 20:30:55 +0530

    Updated UI

--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -1,8 +1,211 @@
 import React, { useState } from "react";
 import axios from 'axios'
 import '../assets/css/surveyform.css'
+import { Select, Input } from 'antd';
 
+
+const { TextArea } = Input;
 const SurveyForm = () => {
+
+
+    const countries = [
+        { value: 'Afghanistan', label: 'Afghanistan' },
+        { value: 'Albania', label: 'Albania' },
+        { value: 'Algeria', label: 'Algeria' },
+        { value: 'Andorra', label: 'Andorra' },
+        { value: 'Angola', label: 'Angola' },
+        { value: 'Antigua and Barbuda', label: 'Antigua and Barbuda' },
+        { value: 'Argentina', label: 'Argentina' },
+        { value: 'Armenia', label: 'Armenia' },
+        { value: 'Australia', label: 'Australia' },
+        { value: 'Austria', label: 'Austria' },
+        { value: 'Azerbaijan', label: 'Azerbaijan' },
+        { value: 'Bahamas', label: 'Bahamas' },
+        { value: 'Bahrain', label: 'Bahrain' },
+        { value: 'Bangladesh', label: 'Bangladesh' },
+        { value: 'Barbados', label: 'Barbados' },
+        { value: 'Belarus', label: 'Belarus' },
+        { value: 'Belgium', label: 'Belgium' },
+        { value: 'Belize', label: 'Belize' },
+        { value: 'Benin', label: 'Benin' },
+        { value: 'Bhutan', label: 'Bhutan' },
+        { value: 'Bolivia', label: 'Bolivia' },
+        { value: 'Bosnia and Herzegovina', label: 'Bosnia and Herzegovina' },
+        { value: 'Botswana', label: 'Botswana' },
+        { value: 'Brazil', label: 'Brazil' },
+        { value: 'Brunei', label: 'Brunei' },
+        { value: 'Bulgaria', label: 'Bulgaria' },
+        { value: 'Burkina Faso', label: 'Burkina Faso' },
+        { value: 'Burundi', label: 'Burundi' },
+        { value: 'Cabo Verde', label: 'Cabo Verde' },
+        { value: 'Cambodia', label: 'Cambodia' },
+        { value: 'Cameroon', label: 'Cameroon' },
+        { value: 'Canada', label: 'Canada' },
+        { value: 'Central African Republic', label: 'Central African Republic' },
+        { value: 'Chad', label: 'Chad' },
+        { value: 'Chile', label: 'Chile' },
+        { value: 'China', label: 'China' },
+        { value: 'Colombia', label: 'Colombia' },
+        { value: 'Comoros', label: 'Comoros' },
+        { value: 'Congo', label: 'Congo' },
+        { value: 'Costa Rica', label: 'Costa Rica' },
+        { value: 'Côte d\'Ivoire', label: 'Côte d\'Ivoire' },
+        { value: 'Croatia', label: 'Croatia' },
+        { value: 'Cuba', label: 'Cuba' },
+        { value: 'Cyprus', label: 'Cyprus' },
+        { value: 'Czech Republic', label: 'Czech Republic' },
+        { value: 'Denmark', label: 'Denmark' },
+        { value: 'Djibouti', label: 'Djibouti' },
+        { value: 'Dominica', label: 'Dominica' },
+        { value: 'Dominican Republic', label: 'Dominican Republic' },
+        { value: 'East Timor (Timor-Leste)', label: 'East Timor (Timor-Leste)' },
+        { value: 'Ecuador', label: 'Ecuador' },
+        { value: 'Egypt', label: 'Egypt' },
+        { value: 'El Salvador', label: 'El Salvador' },
+        { value: 'Equatorial Guinea', label: 'Equatorial Guinea' },
+        { value: 'Eritrea', label: 'Eritrea' },
+        { value: 'Estonia', label: 'Estonia' },
+        { value: 'Eswatini', label: 'Eswatini' },
+        { value: 'Ethiopia', label: 'Ethiopia' },
+        { value: 'Fiji', label: 'Fiji' },
+        { value: 'Finland', label: 'Finland' },
+        { value: 'France', label: 'France' },
+        { value: 'Gabon', label: 'Gabon' },
+        { value: 'Gambia', label: 'Gambia' },
+        { value: 'Georgia', label: 'Georgia' },
+        { value: 'Germany', label: 'Germany' },
+        { value: 'Ghana', label: 'Ghana' },
+        { value: 'Greece', label: 'Greece' },
+        { value: 'Grenada', label: 'Grenada' },
+        { value: 'Guatemala', label: 'Guatemala' },
+        { value: 'Guinea', label: 'Guinea' },
+        { value: 'Guinea-Bissau', label: 'Guinea-Bissau' },
+        { value: 'Guyana', label: 'Guyana' },
+        { value: 'Haiti', label: 'Haiti' },
+        { value: 'Honduras', label: 'Honduras' },
+        { value: 'Hungary', label: 'Hungary' },
+        { value: 'Iceland', label: 'Iceland' },
+        { value: 'India', label: 'India' },
+        { value: 'Indonesia', label: 'Indonesia' },
+        { value: 'Iran', label: 'Iran' },
+        { value: 'Iraq', label: 'Iraq' },
+        { value: 'Ireland', label: 'Ireland' },
+        { value: 'Israel', label: 'Israel' },
+        { value: 'Italy', label: 'Italy' },
+        { value: 'Jamaica', label: 'Jamaica' },
+        { value: 'Japan', label: 'Japan' },
+        { value: 'Jordan', label: 'Jordan' },
+        { value: 'Kazakhstan', label: 'Kazakhstan' },
+        { value: 'Kenya', label: 'Kenya' },
+        { value: 'Kiribati', label: 'Kiribati' },
+        { value: 'Korea, North', label: 'Korea, North' },
+        { value: 'Korea, South', label: 'Korea, South' },
+        { value: 'Kosovo', label: 'Kosovo' },
+        { value: 'Kuwait', label: 'Kuwait' },
+        { value: 'Kyrgyzstan', label: 'Kyrgyzstan' },
+        { value: 'Laos', label: 'Laos' },
+        { value: 'Latvia', label: 'Latvia' },
+        { value: 'Lebanon', label: 'Lebanon' },
+        { value: 'Lesotho', label: 'Lesotho' },
+        { value: 'Liberia', label: 'Liberia' },
+        { value: 'Libya', label: 'Libya' },
+        { value: 'Liechtenstein', label: 'Liechtenstein' },
+        { value: 'Lithuania', label: 'Lithuania' },
+        { value: 'Luxembourg', label: 'Luxembourg' },
+        { value: 'Madagascar', label: 'Madagascar' },
+        { value: 'Malawi', label: 'Malawi' },
+        { value: 'Malaysia', label: 'Malaysia' },
+        { value: 'Maldives', label: 'Maldives' },
+        { value: 'Mali', label: 'Mali' },
+        { value: 'Malta', label: 'Malta' },
+        { value: 'Marshall Islands', label: 'Marshall Islands' },
+        { value: 'Mauritania', label: 'Mauritania' },
+        { value: 'Mauritius', label: 'Mauritius' },
+        { value: 'Mexico', label: 'Mexico' },
+        { value: 'Micronesia', label: 'Micronesia' },
+        { value: 'Moldova', label: 'Moldova' },
+        { value: 'Monaco', label: 'Monaco' },
+        { value: 'Mongolia', label: 'Mongolia' },
+        { value: 'Montenegro', label: 'Montenegro' },
+        { value: 'Morocco', label: 'Morocco' },
+        { value: 'Mozambique', label: 'Mozambique' },
+        { value: 'Myanmar', label: 'Myanmar' },
+        { value: 'Namibia', label: 'Namibia' },
+        { value: 'Nauru', label: 'Nauru' },
+        { value: 'Nepal', label: 'Nepal' },
+        { value: 'Netherlands', label: 'Netherlands' },
+        { value: 'New Zealand', label: 'New Zealand' },
+        { value: 'Nicaragua', label: 'Nicaragua' },
+        { value: 'Niger', label: 'Niger' },
+        { value: 'Nigeria', label: 'Nigeria' },
+        { value: 'North Macedonia', label: 'North Macedonia' },
+        { value: 'Norway', label: 'Norway' },
+        { value: 'Oman', label: 'Oman' },
+        { value: 'Pakistan', label: 'Pakistan' },
+        { value: 'Palau', label: 'Palau' },
+        { value: 'Palestine', label: 'Palestine' },
+        { value: 'Panama', label: 'Panama' },
+        { value: 'Papua New Guinea', label: 'Papua New Guinea' },
+        { value: 'Paraguay', label: 'Paraguay' },
+        { value: 'Peru', label: 'Peru' },
+        { value: 'Philippines', label: 'Philippines' },
+        { value: 'Poland', label: 'Poland' },
+        { value: 'Portugal', label: 'Portugal' },
+        { value: 'Qatar', label: 'Qatar' },
+        { value: 'Romania', label: 'Romania' },
+        { value: 'Russia', label: 'Russia' },
+        { value: 'Rwanda', label: 'Rwanda' },
+        { value: 'Saint Kitts and Nevis', label: 'Saint Kitts and Nevis' },
+        { value: 'Saint Lucia', label: 'Saint Lucia' },
+        { value: 'Saint Vincent and the Grenadines', label: 'Saint Vincent and the Grenadines' },
+        { value: 'Samoa', label: 'Samoa' },
+        { value: 'San Marino', label: 'San Marino' },
+        { value: 'Saudi Arabia', label: 'Saudi Arabia' },
+        { value: 'Senegal', label: 'Senegal' },
+        { value: 'Serbia', label: 'Serbia' },
+        { value: 'Seychelles', label: 'Seychelles' },
+        { value: 'Sierra Leone', label: 'Sierra Leone' },
+        { value: 'Singapore', label: 'Singapore' },
+        { value: 'Slovakia', label: 'Slovakia' },
+        { value: 'Slovenia', label: 'Slovenia' },
+        { value: 'Solomon Islands', label: 'Solomon Islands' },
+        { value: 'Somalia', label: 'Somalia' },
+        { value: 'South Africa', label: 'South Africa' },
+        { value: 'South Sudan', label: 'South Sudan' },
+        { value: 'Spain', label: 'Spain' },
+        { value: 'Sri Lanka', label: 'Sri Lanka' },
+        { value: 'Sudan', label: 'Sudan' },
+        { value: 'Suriname', label: 'Suriname' },
+        { value: 'Sweden', label: 'Sweden' },
+        { value: 'Switzerland', label: 'Switzerland' },
+        { value: 'Syria', label: 'Syria' },
+        { value: 'Taiwan', label: 'Taiwan' },
+        { value: 'Tajikistan', label: 'Tajikistan' },
+        { value: 'Tanzania', label: 'Tanzania' },
+        { value: 'Thailand', label: 'Thailand' },
+        { value: 'Timor-Leste', label: 'Timor-Leste' },
+        { value: 'Togo', label: 'Togo' },
+        { value: 'Tonga', label: 'Tonga' },
+        { value: 'Trinidad and Tobago', label: 'Trinidad and Tobago' },
+        { value: 'Tunisia', label: 'Tunisia' },
+        { value: 'Turkey', label: 'Turkey' },
+        { value: 'Turkmenistan', label: 'Turkmenistan' },
+        { value: 'Tuvalu', label: 'Tuvalu' },
+        { value: 'Uganda', label: 'Uganda' },
+        { value: 'Ukraine', label: 'Ukraine' },
+        { value: 'United Arab Emirates', label: 'United Arab Emirates' },
+        { value: 'United Kingdom', label: 'United Kingdom' },
+        { value: 'United States of America', label: 'United States of America' },
+        { value: 'Uruguay', label: 'Uruguay' },
+        { value: 'Uzbekistan', label: 'Uzbekistan' },
+        { value: 'Vanuatu', label: 'Vanuatu' },
+        { value: 'Vatican City', label: 'Vatican City' },
+        { value: 'Venezuela', label: 'Venezuela' },
+        { value: 'Vietnam', label: 'Vietnam' },
+        { value: 'Yemen', label: 'Yemen' },
+        { value: 'Zambia', label: 'Zambia' },
+        { value: 'Zimbabwe', label: 'Zimbabwe' },
+    ];
     const [formData, setFormData] = useState({
         name: "",
         gender: "Male",
@@ -13,28 +216,30 @@ const SurveyForm = () => {
         message: "",
     });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
+    const handleInputChange = (name, value) => {
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: value,
         }));
     };
 
-    const handleSubmit = async(e) => {
+    const handleChange = (value, name) => {
+        console.log(`Selected ${value} for ${name}`);
+        handleInputChange(name, value);
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const baseUrl = 'https://surveyform-backend-2n7e.onrender.com/'
+        const baseUrl = 'https://surveyform-backend-2n7e.onrender.com/';
         try {
-            const response = await axios.post(baseUrl, formData)
-            if(response.status === 200){
+            const response = await axios.post(baseUrl, formData);
+            if (response.status === 200) {
                 console.log("Data Saved!");
             }
         } catch (error) {
-            console.log("Error", error)
+            console.log("Error", error);
         }
-
-        // Add your form submission logic here using formData
         console.log(formData);
     };
 
@@ -53,83 +258,104 @@ const SurveyForm = () => {
             <form onSubmit={handleSubmit}>
                 <h2>Your Personal Details</h2>
 
-                <div className="form-field">
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleInputChange}
-                    />
+                <div className="row">
+                    <div className="form-field">
+                        <label htmlFor="name">Name</label>
+                        <Input
+                            placeholder="Enter Name"
+                            type="text"
+                            id="name"
+                            name="name"
+                            onChange={(e) => handleInputChange("name", e.target.value)}
+                        />
+                    </div>
+
+                    <div className="form-field">
+                        <label htmlFor="email">Email</label>
+                        <Input
+                            placeholder="Enter Email"
+                            type="email"
+                            id="email"
+                            name="email"
+                            onChange={(e) => handleInputChange("email", e.target.value)}
+                        />
+                    </div>
                 </div>
 
-                <div className="form-field">
-                    <label htmlFor="gender">Gender</label>
-                    <select
-                        name="gender"
-                        id="gender"
-                        value={formData.gender}
-                        onChange={handleInputChange}
-                    >
-                    </select>
-                </div>
+                <div className="row">
+                    <div className="form-field">
+                        <label htmlFor="gender">Gender</label>
+                        <Select
+                            placeholder="Select Gender"
+                            name="gender"
+                            id="gender-field"
+                            defaultValue={formData.gender}
+                            onChange={(value) => handleChange(value, "gender")}
+                            options={[
+                                {
+                                    value: 'Male',
+                                    label: 'Male',
+                                },
+                                {
+                                    value: 'Female',
+                                    label: 'Female',
+                                },
+                                {
+                                    value: 'Others',
+                                    label: 'Others',
+                                },
+                            ]}
+                        />
+                    </div>
 
-                <div className="form-field">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                    />
-                </div>
+                    <div className="form-field">
+                        <label htmlFor="nationality">Nationality</label>
+                        <Select
+                            placeholder="Select Nationality"
+                            name="nationality"
+                            id="nationality-field"
+                            defaultValue={formData.nationality}
+                            onChange={(value) => handleChange(value, "nationality")}
+                            options={countries}
+                        />
+                    </div>
 
-                <div className="form-field">
-                    <label htmlFor="nationality">Nationality</label>
-                    <select
-                        name="nationality"
-                        id="nationality"
-                        value={formData.nationality}
-                        onChange={handleInputChange}
-                    >
-                        {/* Add your nationality options here */}
-                    </select>
-                </div>
-
-                <div className="form-field">
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        value={formData.phone}
-                        onChange={handleInputChange}
-                    />
+                    <div className="form-field">
+                        <label htmlFor="phone">Phone</label>
+                        <Input
+                            placeholder="Enter Phone"
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            value={formData.phone}
+                            onChange={(e) => handleInputChange("phone", e.target.value)}
+                        />
+                    </div>
                 </div>
 
                 <div className="form-field">
                     <label htmlFor="address">Address</label>
-                    <textarea
+                    <TextArea
+                        placeholder="Enter Address"
                         name="address"
                         id="address"
                         value={formData.address}
-                        onChange={handleInputChange}
-                    ></textarea>
+                        onChange={(e) => handleInputChange("address", e.target.value)}
+                    />
                 </div>
 
                 <div className="form-field">
                     <label htmlFor="message">Message</label>
-                    <textarea
+                    <TextArea
+                        placeholder="Enter Message"
                         name="message"
                         id="message"
                         value={formData.message}
-                        onChange={handleInputChange}
-                    ></textarea>
+                        onChange={(e) => handleInputChange("message", e.target.value)}
+                    />
                 </div>
 
-                <button type="submit">Submit Data</button>
+                <button className="submit-form-btn" type="submit">Submit Data</button>
             </form>
         </div>
     );
